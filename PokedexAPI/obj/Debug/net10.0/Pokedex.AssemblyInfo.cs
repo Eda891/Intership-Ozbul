b8715943f2db806d8a5fc2852cea7aafdf29c106
Eda Çalışkan <edacaliskan4402@gmail.com>
2026-07-14 12:10:15 +0300

    Last changes and updates

--- PokedexAPI/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
+++ PokedexAPI/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb7269c63fd439afd4ec3dff716a8f89e0059fc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d64d1f5fbcb4e7c19de528ec06ebbd90fb9138")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
